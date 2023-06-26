ba91cca4074e1e85a3126041206e4792b27b8824
Mrlongtrang <gorkysandy02@gmail.com>
2023-06-26 10:17:09 +0700

    merge them file

--- project.js
+++ project.js
@@ -17,3 +17,18 @@ function myFunction() {
       }
     }
   }
+
+  const product =[
+    {Name:"San Pham 1", Price:"145,000",},
+    {Name:"san Pham 2", price:"99,000"},
+    {Name:"san Pham 3", price:"50,000"},
+    {Name:"san Pham 4", price:"120,000"},
+    {Name:"san Pham 5", price:"125,000"},
+    {Name:"san Pham 6", price:"69,000"},
+    {Name:"san Pham 7", price:"150,000"},
+    {Name:"san Pham 8", price:"67,000"},
+    {Name:"san Pham 9", price:"55,000"},
+    {Name:"san Pham 10", price:"78,000"},
+    {Name:"san Pham 11", price:"100,000"},
+    {Name:"san Pham 12", price:"88,888"},
+  ]
