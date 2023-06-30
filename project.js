8d3e2aecb9d4cea3770e8b9d1cc948b4e5fcb1d7
Mrlongtrang <gorkysandy02@gmail.com>
2023-07-01 00:17:16 +0700

    bao cao chiu

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -18,52 +19,44 @@ function myFunction() {
     }
   }
    
-  let min_price = 49000;
-  let max_price = 190000;
-  $(document).ready(function() {
-    showAllItems ();
+  const allCheckboxes = document.querySelectorAll('input[type=checkbox]');
+  const allProduct = Array.from(document.querySelectorAll('.product-detail'));
+  const checked = {};
+
+  getChecked('high');
+  getChecked('mid');
+  getChecked('low');
+
+  Array.prototype.forEach.call(allCheckboxes, function (el) {
+    el.addEventListener('change', toggleCheckbox);
   });
 
-  $('#min-price').on("change mousemove", function() {
-    min_price = parseInt($('#min-price').val());
-    $('#min-price-txt').text(min_price + "VNĐ");
-    showItemsFiltered();
-  });
-
-  $("#max-price").on("change mousemove", function() {
-    max_price = parseInt($('#max-price').val());
-    $('#max-price-txt').text(max_price + "VNĐ");
-    showItemsFiltered();
-  });
-
-  let category_items = [
-    {thumbnail: "https://media.discordapp.net/attachments/998765310687588423/1121306713384042597/image.png"}
-  ]
-
-  const productPrice = document.getElementsByClassName("product-price");
-  const priceElements = box-product-price.getElementsByClassName("product-price");
-  const price = [];
-
-  for (let i =0;i<priceElements.length;i++) {
-    const price = parseFloat(priceElements[i].textContent);
-    price.push(price);
+  function toggleCheckbox(e) {
+    getChecked(e.target.name);
+    setVisibility();
   }
-  const filterPrice = price
+
+  function getChecked(name) {
+    checked[name] = Array.from(document.querySelectorAll('input[name=' + name + ']:checked')).map(function
+      (el) {
+        return el.value;
+      } );
+  }
   
-
-  function showAllItems() {
-    $("#display-items-div").empty();
-    for (let i = 49000; i <category_items.length; i++) {
-      let items_content = '<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 product-card" data-availiable-sizes="' +category_items[i] ['sizes'] + '"><b>' + category_items[i] ['title'] + '</b><br><img src"' + category_items[i] ['thumbnail'] + '" height="64" width="64" alt="san pham salad"><p>$' + category_items[i]['price'] + '</p></div>';
-    }
-  }
-
-  function showItemsFiltered() {
-    $("#display-items-div").empty();
-    for (let i = 0; i < category_items.length; i++) {
-      if (category_items[i]['price'] <= max_price && category_items[i]['Price']>= min_price) {
-        let item_content = '<div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 product-card" data-availiable-sizes="' +category_items[i] ['sizes'] + '"><b>' + category_items[i]['title'] + '</b><br><img src"' +category_items[i] ['thumbnail'] + '" height="64" width="64" alt="san pham salad"><p>$' +category_items[i][price] + '</p></div>';
-        $("#display-items-div").append(item_content);
+  function setVisibility() {
+    allProduct.map(function (el) {
+      const high = checked.high.length ? _.intersection(Array.from(el.classList),
+      checked.high).length : true;
+      const mid = checked.mid.length ? _.intersection(Array.from(el.classList),
+      checked.mid).length : true;
+      const low = checked.low.length ? _.intersection(Array.from(el.classList),
+      checked.low).length : true;
+      
+      if (high && mid && low) {
+        el.style.display = 'block';
+      } else {
+        el.style.display = 'none'
       }
-    }
+    });
   }
+  
